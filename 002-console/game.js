dfc61681fe5a5a8561d9ae1bab8e03ae6db2e31c
Andrey Tarasov <chir33d@mail.ru>
2025-12-12 11:15:07 +0300

    завершение программы  game

--- 002-console/game.js
+++ 002-console/game.js
@@ -1,15 +1,7 @@
 #!/usr/bin/env node
 
-// function getRandomInt() {
-
-// }
-
-// console.log('get play!');
-
 const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
-const { log } = require('node:console');
-const { number } = require('yargs');
 
 const rl = readline.createInterface({ input, output });
 
@@ -22,47 +14,60 @@ function askQuestion(query) {
 }
 
 async function main() {
-    // const lowAnswer = await askQuestion('Введине нижнюю границу диапазона (число): ');
     let lowNumber;
     let hideNumber;
+    let customNumber;
 
     while (true) {
-        lowNumber = await askQuestion('Введине нижнюю границу диапазона (число): ');
-        const num = Number(lowNumber);
-        if (!isNaN(num)) {
-            console.log(`Спасибо! Вы ввели число: ${num}`);
+        const num = await askQuestion('Введине нижнюю границу диапазона (число): ');
+        lowNumber = Number(num);
+        if (!isNaN(lowNumber)) {
+            console.log(`Спасибо! Вы ввели число: ${lowNumber}`);
             break;
         } else {
-            console.log('Некорректный ввод, попробуйте ещё раз.');
+            console.log('Некорректный ввод, введите число');
         }
     }
 
     while (true) {
-        hideNumber = await askQuestion('Введине верхнюю границу диапазона (число): ');
-        const num = Number(hideNumber);
-        if (isNaN(num)) {
+        const num = await askQuestion('Введине верхнюю границу диапазона (число): ');
+        hideNumber = Number(num);
+        if (isNaN(hideNumber)) {
             console.log('Некорректный ввод, введите число');
         } else if (hideNumber < lowNumber) {
             console.log('Некорректный ввод, верхняя граница должна быть больше');
         } else {
-            console.log(`Спасибо! Вы ввели число: ${num}`);
+            console.log(`Спасибо! Вы ввели число: ${hideNumber}`);
             break;
         }
     }
 
-    console.log(lowNumber, hideNumber);
-    
+    const min = Math.ceil(lowNumber);
+    const max = Math.floor(hideNumber);
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    while (true) {
+        const num = await askQuestion(`Загадано число в диапазоне от ${min} до ${max}: `);
+        customNumber = Number(num);
+        if (isNaN(num)) {
+            console.log('Некорректный ввод, введите число');
+        } else {
+            break;
+        }
+    }
+
+    while (true) {
+        if (customNumber < randomNumber) {
+            customNumber = await askQuestion('Больше: ');
+        } else if (customNumber > randomNumber) {
+            customNumber = await askQuestion('Меньше: ');
+        } else {
+            console.log(`Отгадано число ${customNumber}`);
+            break;
+        }
+    }
 
     rl.close();
 }
 
 main();
-
-// rl.question('Введите нижнюю границу диапазона', (number) => {
-  
-//     rl.on('line', (input) => {
-//         console.log(`Нижняя граница ${number}`);
-//     }); 
-
-//     rl.close();
-// });
