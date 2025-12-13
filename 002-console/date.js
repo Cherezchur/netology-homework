00707dd88c901c553577145cbda59c8994905e9e
Andrey Tarasov <chir33d@mail.ru>
2025-12-13 18:30:08 +0300

    правки и рефакторинг скрипта с датами

--- 002-console/date.js
+++ 002-console/date.js
@@ -3,68 +3,94 @@
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 
-function showPastOrfutureDate(argv, newDate, isPast = false) {
-    if (argv.year) {
-        if (typeof(argv.year) !== 'number') {
+const ALIASES = {
+    year: 'y',
+    month: 'm',
+    date: 'd'
+}
+
+function showPastOrfutureDate(argv, isPast = false) {
+    const errors = [];
+    const data = argv.argv;
+    let changetDate = new Date();
+
+    if (!data[ALIASES.year] && !data[ALIASES.month] && !data[ALIASES.date]) {
+        console.error('Необхидимы ввести флаг(и) и аргумент(ы), попробуйте заново');
+        errors.push('Необхидимы ввести флаг(и) и аргумент(ы), попробуйте заново');
+        return;
+    };
+
+    const checkArgvValue = (value) => {
+        if (typeof(value) !== 'number') {
             console.error('Аргумент должен быть числом');
-            return;
+            errors.push('Аргумент должен быть числом');
+            return false;
         }
+        return true;
+    }
 
-        const futureDate = isPast
-            ? newDate.setFullYear(newDate.getFullYear() - argv.year)
-            : newDate.setFullYear(newDate.getFullYear() + argv.year)
+    const setChangeDate = (alias) => {      
+        console.log('setChangeDate');
+        
+        let currentDate = new Date();
 
-        console.log(new Date(futureDate));
-    } else if (argv.month) {
-        if (typeof(argv.month) !== 'number') {
-            console.error('Аргумент должен быть числом');
-            return;
+        const getTargetValue = (currentValue, argvValue) => isPast
+            ? currentValue - argvValue
+            : currentValue + argvValue;
+
+        if (alias === ALIASES.year) {
+            const targetValue = getTargetValue(currentDate.getFullYear(), data[alias]);
+            changetDate.setFullYear(targetValue);
+        } else if (alias === ALIASES.month) {
+            const targetValue = getTargetValue(currentDate.getMonth(), data[alias]);
+            changetDate.setMonth(targetValue);
+        } else if (alias === ALIASES.date) {
+            const targetValue = getTargetValue(currentDate.getDate(), data[alias]);
+            changetDate.setDate(targetValue);
         }
+    }
+    
+    if (data[ALIASES.year] && checkArgvValue(data[ALIASES.year]) && !errors.length) {
+        setChangeDate(ALIASES.year);
+    }
 
-        const futureDate = isPast
-            ? newDate.setMonth(newDate.getMonth() - argv.month)
-            : newDate.setMonth(newDate.getMonth() + argv.month)
+    if (data[ALIASES.month] && checkArgvValue(data[ALIASES.month]) && !errors.length) {
+        setChangeDate(ALIASES.month);
+    }
 
-        console.log(new Date(futureDate));
-    } else if (argv.date) {
-        if (typeof(argv.date) !== 'number') {
-            console.error('Аргумент должен быть числом');
-            return;
-        }
+    if (data[ALIASES.date] && checkArgvValue(data[ALIASES.date]) && !errors.length) {
+        setChangeDate(ALIASES.date);
+    }
 
-        const futureDate = isPast
-            ? newDate.setDate(newDate.getDate() - argv.date)
-            : newDate.setDate(newDate.getDate() + argv.date)
-
-        console.log(new Date(futureDate));
-    } else {
-        console.error('Необходимо ввести флаг и число');
+    if (!errors.length) {
+        console.log(changetDate);
     }
 }
 
 yargs(hideBin(process.argv))
     .option('year',{
-        alias: 'y',
+        alias: ALIASES.year,
         description: 'show the current year for "current" command or date in the past or future (an argument is needed)',
     })
     .option('month', {
-        alias: 'm',
+        alias: ALIASES.month,
         description: 'show the current month for "current" command or date in the past or future (an argument is needed)',
     })
     .option('date', {
-        alias: 'd',
+        alias: ALIASES.date,
         description: 'show the current  date for "current" command or date in the past or future (an argument is needed)',
     })
     .command(
         'current',
         'shows current date',
-        (yargs) => {
-            const argv = yargs.argv;
-            if (argv.year) {
+        (argv) => {
+            const data = argv.argv;
+            
+            if (data[ALIASES.year]) {
                 console.log(new Date().getFullYear());
-            } else if (argv.month) {
+            } else if (data[ALIASES.month]) {
                 console.log(new Date().getMonth() + 1);
-            } else if (argv.date) {
+            } else if (data[ALIASES.date]) {
                 console.log(new Date().getDate());
             } else {
                 console.log(new Date().toISOString())
@@ -74,23 +100,15 @@ yargs(hideBin(process.argv))
     .command(
         'add',
         'shows date in the future',
-        (yargs) => {
-            const argv = yargs.argv;
-            const now = new Date();
-            const newDate = new Date(now);
-
-            showPastOrfutureDate(argv, newDate);
+        (argv) => {          
+            showPastOrfutureDate(argv, false);
         }
     )
     .command(
         'sub',
         'shows date in the past',
-        (yargs) => {
-            const argv = yargs.argv;
-            const now = new Date();
-            const newDate = new Date(now);
-
-            showPastOrfutureDate(argv, newDate, true);
+        (argv) => {          
+            showPastOrfutureDate(argv, true);
         }
     )
     .help()
